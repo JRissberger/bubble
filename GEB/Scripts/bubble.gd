9aca3b1aa37858c1db73767159e9ace9042cb22a
Sarah Schneider <143359626+ses011@users.noreply.github.com>
2025-01-25 12:43:49 -0500

    bubble constructor & get/set

--- GEB/Scripts/bubble.gd
+++ GEB/Scripts/bubble.gd
@@ -1,9 +1,27 @@
 class_name bubble extends RigidBody2D
 
-var atk = 1
-var hp = 5
-var radius = 15;
-var spd_mult = 5;
+var atk:
+	get: 
+		return atk
+	set(value):
+		atk = value
+var hp:
+	get: 
+		return hp
+	set(value):
+		hp = value
+var spd_mult:
+	get: 
+		return spd_mult
+	set(value):
+		spd_mult = value	
+
+var radius
+
+
+func _init(hp=5, spd_mult=5, atk=1, radius=70):
+	pass
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	apply_central_force(Vector2(randf_range(-1, 1), randf_range(-1, 1)) * spd_mult)
