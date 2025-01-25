94ae610aeaef44a76f8255a26b3ab16653a89b13
JRissberger <jbr4368@rit.edu>
2025-01-25 17:44:29 -0500

    added popup and scene transition

---
+++ GEB/Scripts/popup.gd
@@ -0,0 +1,23 @@
+extends ColorRect
+#text for the "bubble created" popup
+var popupText;
+var arraySize = 0;
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	popupText = get_node("Body");
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	#gets array size to access proper bubble data
+	arraySize = BubbleManager.bubbles.size();
+	#updates popup text
+	popupText.text = ("Health: " + str(BubbleManager.bubbles[arraySize-1].Hp) + "\nAttack: " + 
+	str(BubbleManager.bubbles[arraySize-1].Atk) + "\nSpeed: " + str(BubbleManager.bubbles[arraySize-1].Spd_mult)
+	+ "\n\n\nSelect BATTLE! to enter the arena!");
+	#displays popup
+	self.visible = true;
