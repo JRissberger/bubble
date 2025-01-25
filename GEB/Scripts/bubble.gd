dd1a3dcc9a0f61b25e25ec8322e7e99c2275599b
Sarah Schneider <143359626+ses011@users.noreply.github.com>
2025-01-25 16:37:23 -0500

    health decrease when hitting other bubble

--- GEB/Scripts/bubble.gd
+++ GEB/Scripts/bubble.gd
@@ -1,5 +1,6 @@
 class_name bubble extends RigidBody2D
 var atk
+var health
 var hp
 var spd_mult
 var radius
@@ -22,6 +23,7 @@ var Spd_mult:
 
 
 func _init(hp=5, spd_mult=5, atk=1, radius=70):
+	self.health = hp
 	self.hp = hp
 	self.spd_mult = spd_mult
 	self.atk = atk
@@ -31,6 +33,10 @@ func _init(hp=5, spd_mult=5, atk=1, radius=70):
 func _ready() -> void:
 	apply_central_force(Vector2(randf_range(-1, 1), randf_range(-1, 1)) * spd_mult)
 
+
+func hit(attack: int) -> void:
+	self.health -= attack
+
 func _process(delta: float) -> void:
 	apply_central_force(linear_velocity.normalized() * 0.02)
 
@@ -39,3 +45,8 @@ func _physics_process(delta: float) -> void:
 	var collis = move_and_collide(linear_velocity * delta)
 	if collis:
 		linear_velocity = linear_velocity.bounce(collis.get_normal())
+		if collis.get_collider().has_method("hit"):
+			collis.get_collider().hit(atk)
+		if health <= 0:
+			pass	#TODO: when health 0, ????
+		
