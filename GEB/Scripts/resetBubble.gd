49baa252351b236589b41ad67464a9e1eb330abf
JRissberger <jbr4368@rit.edu>
2025-01-25 21:10:51 -0500

    fixed a metadata thing I missed, hide reset button when popup exists

--- GEB/Scripts/resetBubble.gd
+++ GEB/Scripts/resetBubble.gd
@@ -1,13 +1,17 @@
 extends ColorRect
 
+var popup;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	popup = get_node("../Popup");
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+		#hides the reset button if needed
+	if popup.visible:
+		self.visible = false;
 	pass
 
 
